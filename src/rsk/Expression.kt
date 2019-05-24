61acf06bb5c4aeb62ef245df90eb6e0aa39c6a95
John Furtado <john.furtado@virginpulse.com>
2019-05-24 15:46:28 -0400

    extension functions

--- src/rsk/Expression.kt
+++ src/rsk/Expression.kt
@@ -1,7 +1,19 @@
 package rsk
 
 fun main(args: Array<String>) {
-    println(max(1, 2))
+    val text = "With    multiple \t whitespace"
+    println(replaceMultipleWhiteSpace(text))
+    println(text.replaceMultipleWhiteSpaceEx())
 }
 
 fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+fun replaceMultipleWhiteSpace(value: String) : String {
+    var regex = Regex("\\s+")
+    return regex.replace(value, " ")
+}
+
+fun String.replaceMultipleWhiteSpaceEx() : String {
+    var regex = Regex("\\s+")
+    return regex.replace(this, " ")
+}
